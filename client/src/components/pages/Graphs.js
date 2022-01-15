a05bf88d07854207794489eec9ec457ce5c277f0
Jakob de Raaij <jakob@dhcp-10-29-113-156.dyn.mit.edu>
2022-01-15 17:00:07 -0500

    minor changes

--- client/src/components/pages/Graphs.js
+++ client/src/components/pages/Graphs.js
@@ -114,56 +114,6 @@ const Graphs = ({ userId }) => {
     } */
   });
 
-  /* d3.json("/api/graph").then((json) => {
-      console.log(json);
-      //d3.force.nodes(json.nodes).links(json.links).start();
-
-      let link = svg
-        .selectAll(".link")
-        .data(json.links)
-        .enter()
-        .append("line")
-        .attr("class", "link")
-        .style("stroke-width", function (d) {
-          return Math.sqrt(d.weight);
-        });
-      console.log("Ja");
-
-      let node = svg.selectAll(".node").data(json.nodes).enter().append("g").attr("class", "node");
-      // .call(force.drag);
-
-      node.append("circle").attr("r", "5");
-
-      node
-        .append("text")
-        .attr("dx", 12)
-        .attr("dy", ".35em")
-        .text(function (d) {
-          return d.name;
-        });
-
-       force.on("tick", function () {
-        link
-          .attr("x1", function (d) {
-            return d.source.x;
-          })
-          .attr("y1", function (d) {
-            return d.source.y;
-          })
-          .attr("x2", function (d) {
-            return d.target.x;
-          })
-          .attr("y2", function (d) {
-            return d.target.y;
-          });
-
-        node.attr("transform", function (d) {
-          return "translate(" + d.x + "," + d.y + ")";
-        });
-      }); 
-    });
-  }); */
-
   return (
     <div id="main" ref={main}>
       {" "}
