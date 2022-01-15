66fc7ec7e2ae5f4e371a633056ae6406aa6a3de7
Jakob de Raaij <jakob@dhcp-10-29-113-156.dyn.mit.edu>
2022-01-15 15:45:09 -0500

    basic graph display working

--- client/src/components/pages/Graphs.js
+++ client/src/components/pages/Graphs.js
@@ -1,6 +1,6 @@
 import React, { Component, useEffect, useState } from "react";
 import * as d3 from "d3";
-import { forceSimulation } from "https://cdn.skypack.dev/d3-force@3";
+//import { forceSimulation } from "https://cdn.skypack.dev/d3-force@3";
 
 import "../../utilities.css";
 import "./Graphs.css";
@@ -11,71 +11,78 @@ const Graphs = ({ userId }) => {
   const HEIGHT = 500;
   const data = [2, 3, 4, 1];
 
-  //const svg = d3.select("#svg").append("svg").attr("width", WIDTH).attr("height", HEIGHT);
+  /* const svg = d3
+    .select("#svg")
+    .append("svg")
+    .attr("width", WIDTH)
+    .attr("height", HEIGHT)
+    .style("background-color", "red"); */
+  //  const width = svg.attr("width");
+  //const height = svg.attr("height");
 
   useEffect(() => {
-    const accessToRef = d3
+    const svg = d3
       .select(main.current)
       .append("svg")
       .attr("width", WIDTH)
       .attr("height", HEIGHT)
-      .style("background-color", "blue");
-    accessToRef.style("color", "red");
-    accessToRef
-      .selectAll("rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("height", (d, i) => d * 50)
-      .attr("width", "100px")
-      .attr("x", (d, i) => i * 110)
-      .attr("y", (d, i) => HEIGHT - d * 50)
-      .style("background-color", "red");
+      .style("background-color", "white");
 
-    const links = [
-      { source: 2, target: 1, weight: 1 },
-      { source: 0, target: 2, weight: 3 },
-    ];
+    const graph = {
+      links: [
+        { source: "2", target: "1", weight: 1 },
+        { source: "3", target: "2", weight: 3 },
+      ],
 
-    const nodes = [
-      { name: "node1", group: 1 },
-      { name: "node2", group: 2 },
-      { name: "node3", group: 2 },
-      { name: "node4", group: 3 },
-    ];
+      nodes: [{ name: "1" }, { name: "2" }, { name: "3" }, { name: "4" }],
+    };
 
     /* links.forEach(function (link) {
       link.source = nodes[link.source] || (nodes[link.source] = { name: link.source });
       link.target = nodes[link.target] || (nodes[link.target] = { name: link.target });
     }); */
 
-    let force = d3.forceSimulation
+    let simulation = d3
+      .forceSimulation(graph.nodes)
+      .force(
+        "link",
+        d3
+          .forceLink()
+          .id(function (d) {
+            return d.name;
+          })
+          .links(graph.links)
+      )
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(WIDTH / 2, HEIGHT / 2))
+      .on("tick", ticked);
 
-      .nodes(nodes)
-      .links(links)
-      .on("tick", tick)
-      .linkDistance(300)
-      .start();
+    let link = svg
+      .append("g")
+      .selectAll("line")
+      .data(graph.links)
+      .enter()
+      .append("line")
+      .attr("stroke-width", 5)
+      .attr("stroke", "grey");
 
-    let link = svg.selectAll(".link").data(links).enter().append("line").attr("class", "link");
     let node = svg
-      .selectAll(".node")
-      .data(force.nodes())
+      .append("g")
+      .selectAll("circle")
+      .data(graph.nodes)
       .enter()
       .append("circle")
-      .attr("class", "node")
-      .attr("r", (width = 0.03));
+      .attr("r", 10)
+      .attr("fill", "black");
 
-    function tick(e) {
+    function ticked() {
       node
         .attr("cx", function (d) {
           return d.x;
         })
         .attr("cy", function (d) {
           return d.y;
-        })
-        .call(force.drag);
-
+        });
       link
         .attr("x1", function (d) {
           return d.source.x;
