da5bca5d242cbe298b0406c58e8b44d976109baa
odahleh <omardahleh@gmail.com>
2022-01-14 21:31:43 -0500

    landing page made

--- client/src/components/pages/Index.js
+++ client/src/components/pages/Index.js
@@ -25,22 +25,15 @@ const Index = ({ userId, handleLogin, handleLogout }) => {
           onFailure={(err) => console.log(err)}
         />
       )}
-      <h1>Good luck on your project :)</h1>
-      <h2> What you need to change in this skeleton</h2>
-      <ul>
-        <li>
-          Change the Frontend CLIENT_ID (Skeleton.js) to your team's CLIENT_ID (obtain this at
-          http://weblab.to/clientid)
-        </li>
-        <li>Change the Server CLIENT_ID to the same CLIENT_ID (auth.js)</li>
-        <li>
-          Change the Database SRV (mongoConnectionURL) for Atlas (server.js). You got this in the
-          MongoDB setup.
-        </li>
-        <li>Change the Database Name for MongoDB to whatever you put in the SRV (server.js)</li>
-      </ul>
-      <h2>How to go from this skeleton to our actual app</h2>
-      <a href="http://weblab.to/get-started">Check out this getting started guide</a>
+      <div class="landing-page-container">
+        <h1 class="landing-page-title">Homepage Name</h1>
+        <h2 class="landing-page-content">
+          {" "}
+          Homepage descirption Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod
+          tempor incidunt ut labore et dolore magna aliqua.{" "}
+        </h2>
+        <button class="landing-page-button landing-page-content">Get Started!</button>
+      </div>
     </>
   );
 };
